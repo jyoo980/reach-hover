2f9b1415bc79b421bc0ed6adfa05476654ec71d9
James Yoo <jyoo980@gmail.com>
2022-02-16 20:12:13 -0800

    Adding dependency on Spotless for linting (#1)
    * `spotless` is being added as a Gradle dependency.
    * Linting must now succeed before merging to `main` is permitted.

--- src/main/kotlin/com/github/jyoo980/reachhover/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/services/MyProjectService.kt
@@ -1,7 +1,7 @@
 package com.github.jyoo980.reachhover.services
 
-import com.intellij.openapi.project.Project
 import com.github.jyoo980.reachhover.MyBundle
+import com.intellij.openapi.project.Project
 
 class MyProjectService(project: Project) {
 
