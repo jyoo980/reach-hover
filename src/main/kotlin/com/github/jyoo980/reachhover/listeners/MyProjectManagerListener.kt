257cae0229c9785713cbf1c0447504466c97963d
James Yoo <jyoo980@gmail.com>
2022-02-16 19:41:38 -0800

    Fixing lint errors

--- src/main/kotlin/com/github/jyoo980/reachhover/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
 package com.github.jyoo980.reachhover.listeners
 
+import com.github.jyoo980.reachhover.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.jyoo980.reachhover.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
