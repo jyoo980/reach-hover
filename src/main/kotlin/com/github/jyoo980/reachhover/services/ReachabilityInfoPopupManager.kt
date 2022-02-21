d71c3582f9bbdba3de24e8e7f4b5c69819724e48
James Yoo <jyoo980@gmail.com>
2022-02-21 15:22:58 -0800

    Removing unused function ReachabilityInfoPopupManager#component

--- src/main/kotlin/com/github/jyoo980/reachhover/services/ReachabilityInfoPopupManager.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/services/ReachabilityInfoPopupManager.kt
@@ -1,17 +1,11 @@
 package com.github.jyoo980.reachhover.services
 
 import com.github.jyoo980.reachhover.model.ReachabilityHoverContext
-import com.github.jyoo980.reachhover.ui.BackwardReachabilityButton
 import com.github.jyoo980.reachhover.ui.ReachabilityPopupBuilder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import java.awt.GridLayout
 import java.lang.ref.WeakReference
-import javax.swing.JButton
-import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.SwingConstants
 
 class ReachabilityInfoPopupManager {
 
@@ -39,22 +33,5 @@ class ReachabilityInfoPopupManager {
             this.currentPopupRef = WeakReference(popup)
             popup.show(it.location)
         }
-    }
-
-    private fun component(): JComponent {
-        val panel = JPanel(GridLayout(2, 1))
-        val documentationButton =
-            JButton("Show types and documentation").also {
-                it.horizontalAlignment = SwingConstants.LEFT
-                it.isBorderPainted = false
-                it.isContentAreaFilled = false
-            }
-        this.optCurrentContext?.also {
-            val reachabilityButton = BackwardReachabilityButton(it.elementToInspect)
-            reachabilityButton.setButtonText()
-            panel.add(reachabilityButton.ui)
-        }
-        panel.add(documentationButton)
-        return panel
     }
 }
