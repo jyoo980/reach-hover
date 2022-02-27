a38a9431ef25dcc9178d4edc1013fd45752499fe
James Yoo <jyoo980@gmail.com>
2022-02-27 13:11:17 -0800

    Applying Spotless lint fixes

--- src/main/kotlin/com/github/jyoo980/reachhover/util/PsiExtension.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/util/PsiExtension.kt
@@ -6,11 +6,10 @@ import com.intellij.psi.util.PsiTreeUtil
 /**
  * Extension method on PsiElement.
  *
- * INVARIANT:
- * In the event that the cursor is hovering over an anon. class or lambda expr,
- * the location of the first PsiExpressionList object must be closer than that
- * of the first PsiAnonymousClass or PsiLambdaExpression, otherwise we have
- * PsiElements that are not method arguments being reported as such.
+ * INVARIANT: In the event that the cursor is hovering over an anon. class or lambda expr, the
+ * location of the first PsiExpressionList object must be closer than that of the first
+ * PsiAnonymousClass or PsiLambdaExpression, otherwise we have PsiElements that are not method
+ * arguments being reported as such.
  *
  * @return true iff the element is an instance of a non-literal method argument, e.g., foo(a, 1),
  * a.isNonLiteralMethodArg() -> true e.g., foo(a, 1), 1.isNonLiteralMethodArg() -> false
