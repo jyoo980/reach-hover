3ac101a01c3651c2501e4b56ab40bd6b30436a4b
James Yoo <jyoo980@gmail.com>
2022-02-27 16:24:57 -0800

    Adding basic delay to hover popup UI
      * Using the same hovering delay as native IntelliJ popups.
      * Still need to find a way to remove the original popup from appearing.

--- src/main/kotlin/com/github/jyoo980/reachhover/listeners/EditorHoverListener.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/listeners/EditorHoverListener.kt
@@ -1,20 +1,48 @@
 package com.github.jyoo980.reachhover.listeners
 
+import com.github.jyoo980.reachhover.model.ReachabilityHoverContext
+import com.github.jyoo980.reachhover.services.ReachabilityInfoPopupManager
 import com.github.jyoo980.reachhover.util.MouseHoverUtil
+import com.github.jyoo980.reachhover.util.isLocalVariableReference
+import com.github.jyoo980.reachhover.util.isNonLiteralMethodArg
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
+import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.ui.awt.RelativePoint
+import com.intellij.util.Alarm
+import kotlin.math.max
 
 internal class EditorHoverListener : EditorMouseMotionListener {
 
     private val logger: Logger = Logger.getInstance(EditorHoverListener::class.java)
+    private val reachabilityPopupManager: ReachabilityInfoPopupManager =
+        ReachabilityInfoPopupManager()
+    private val alarm: Alarm = Alarm()
 
     override fun mouseMoved(e: EditorMouseEvent) {
-        val offset = MouseHoverUtil.targetOffset(e) ?: return
-        val project = e.editor.project ?: return
-        val optElement = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
-        // TODO: 2022-02-17 Add check whether optElement is the item we're looking for
-        val elementUnderInspection = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
-        // (variable/method arg).
+        val actionStartTimeMs = System.currentTimeMillis()
+        alarm.addRequest({ handleMouseEvent(e) }, actionDelayTime(actionStartTimeMs))
+    }
+
+    private fun handleMouseEvent(e: EditorMouseEvent) {
+        MouseHoverUtil.targetOffset(e)?.let { offset ->
+            val project = e.editor.project ?: return
+            val elementToAnalyze = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
+            elementToAnalyze
+                ?.takeIf { it.isNonLiteralMethodArg() || it.isLocalVariableReference() }
+                ?.let {
+                    val context = ReachabilityHoverContext(it, RelativePoint(e.mouseEvent))
+                    reachabilityPopupManager.showReachabilityPopupFor(context)
+                }
+        }
+    }
+
+    private fun actionDelayTime(startTimeMs: Long): Long {
+        return max(
+            0,
+            EditorSettingsExternalizable.getInstance().tooltipsDelay -
+                (System.currentTimeMillis() - startTimeMs)
+        )
     }
 }
