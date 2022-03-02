29ed8d7b2fce2bbc9bf8fff6fd4d0a50081cd11c
James Yoo <jyoo980@gmail.com>
2022-03-01 17:36:51 -0800

    Adding base reachability actions (#8)
    * Adding initial popup action behaviour
    
      * Ensuring that only one reachability popup is available at a time.
      * Still need to wire-up the actual reachability action invocation.
      * Need to find a way to close the popup on mousing away.
    
    * Wiring up slicer with reachability hover buttons
    
      * Slicer action is now invoked when buttons in the popup are selected.
      * Need to figure out why the slicer appears to not be slicing the correct
        element.
    
    * Adding check to prevent useless popup calls
    
      * Now only making calls to the popup manager for PsiElement objects of the
        correct type.
    
    * Tying the popup to the base reachability vis

--- src/main/kotlin/com/github/jyoo980/reachhover/listeners/EditorHoverListener.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/listeners/EditorHoverListener.kt
@@ -1,20 +1,33 @@
 package com.github.jyoo980.reachhover.listeners
 
+import com.github.jyoo980.reachhover.model.ReachabilityHoverContext
+import com.github.jyoo980.reachhover.services.ReachabilityInfoPopupManager
 import com.github.jyoo980.reachhover.util.MouseHoverUtil
+import com.github.jyoo980.reachhover.util.isLocalVariableReference
+import com.github.jyoo980.reachhover.util.isNonLiteralMethodArg
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
+import com.intellij.ui.awt.RelativePoint
 
 internal class EditorHoverListener : EditorMouseMotionListener {
 
     private val logger: Logger = Logger.getInstance(EditorHoverListener::class.java)
+    private val reachabilityPopupManager: ReachabilityInfoPopupManager =
+        ReachabilityInfoPopupManager()
 
     override fun mouseMoved(e: EditorMouseEvent) {
-        val offset = MouseHoverUtil.targetOffset(e) ?: return
-        val project = e.editor.project ?: return
-        val optElement = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
-        // TODO: 2022-02-17 Add check whether optElement is the item we're looking for
-        val elementUnderInspection = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
-        // (variable/method arg).
+        MouseHoverUtil.targetOffset(e)?.let { offset ->
+            val project = e.editor.project ?: return
+            val elementUnderInspection = MouseHoverUtil.elementAtOffset(project, e.editor, offset)
+            elementUnderInspection
+                ?.takeIf { it.isNonLiteralMethodArg() || it.isLocalVariableReference() }
+                ?.let {
+                    val context =
+                        ReachabilityHoverContext(it, RelativePoint(e.mouseEvent), e.editor)
+                    reachabilityPopupManager.showReachabilityPopupFor(context)
+                }
+        }
+            ?: reachabilityPopupManager.resetPopupState()
     }
 }
--- src/main/kotlin/com/github/jyoo980/reachhover/model/ReachabilityHoverContext.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/model/ReachabilityHoverContext.kt
@@ -1,6 +1,11 @@
 package com.github.jyoo980.reachhover.model
 
+import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.ui.awt.RelativePoint
 
-data class ReachabilityHoverContext(val elementToInspect: PsiElement, val location: RelativePoint)
+data class ReachabilityHoverContext(
+    val elementToInspect: PsiElement,
+    val location: RelativePoint,
+    val editor: Editor
+)
