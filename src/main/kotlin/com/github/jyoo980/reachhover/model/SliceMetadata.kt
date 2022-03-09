0e9c7e021a42ecf9d0284218d6af5fea84aa214f
James Yoo <jyoo980@gmail.com>
2022-03-09 10:51:29 -0800

    Apply Spotless fixes

--- src/main/kotlin/com/github/jyoo980/reachhover/model/SliceMetadata.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/model/SliceMetadata.kt
@@ -3,8 +3,4 @@ package com.github.jyoo980.reachhover.model
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 
-data class SliceMetadata(
-    val element: PsiElement?,
-    val file: PsiFile?,
-    val lineInFile: Int?
-)
+data class SliceMetadata(val element: PsiElement?, val file: PsiFile?, val lineInFile: Int?)
