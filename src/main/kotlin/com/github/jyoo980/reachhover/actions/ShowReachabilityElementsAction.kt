407bb172c677de1266ce92d122c285d5496ce55f
James Yoo <jyoo980@gmail.com>
2022-04-06 17:13:47 -0700

    Improving performance of reach-hover (#32)
    * Main issue was the recursive tree walks that were happening. The result of
        these weren't being used anyway.
      * Project-level analyses works fine, works for `intellij-community`, which
        has ~6.69MM LOC for Java.

--- src/main/kotlin/com/github/jyoo980/reachhover/actions/ShowReachabilityElementsAction.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/actions/ShowReachabilityElementsAction.kt
@@ -1,23 +1,16 @@
 package com.github.jyoo980.reachhover.actions
 
-import com.github.jyoo980.reachhover.codeinsight.ReachabilityElementViewSession
 import com.github.jyoo980.reachhover.model.ReachabilityContext
-import com.github.jyoo980.reachhover.model.map
 import com.github.jyoo980.reachhover.ui.ReachabilityPanel
-import com.github.jyoo980.reachhover.ui.ReachabilityViewElement
 import com.intellij.codeInsight.documentation.DocumentationManager
-import com.intellij.codeInsight.hint.ImplementationViewComponent
-import com.intellij.codeInsight.hint.ImplementationViewSession
 import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.ide.DataManager
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.ui.popup.ActiveIcon
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.reference.SoftReference
 import com.intellij.slicer.SliceNode
-import com.intellij.ui.popup.AbstractPopup
 import com.intellij.ui.popup.PopupPositionManager
 import com.intellij.ui.popup.PopupUpdateProcessor
 import icons.IconManager
@@ -26,7 +19,6 @@ import java.lang.ref.WeakReference
 
 class ShowReachabilityElementsAction {
 
-    private val logger: Logger = Logger.getInstance(ShowReachabilityElementsAction::class.java)
     private var popupRef: Reference<JBPopup>? = null
 
     fun performForContext(
@@ -34,75 +26,61 @@ class ShowReachabilityElementsAction {
         root: SliceNode,
         dataflowFromHere: Boolean
     ) {
-        val (editor, exprUnderInspection, questionText, tree) = context
-        val reachabilityViewElements =
-            tree.map { metadata -> metadata.element?.let { ReachabilityViewElement(it) }!! }
-        ReachabilityElementViewSession(editor, exprUnderInspection, reachabilityViewElements).also {
-            showReachabilitySession(it, editor, root, questionText, dataflowFromHere)
-        }
+        val (editor, _, questionText) = context
+        showReachabilitySession(editor, root, questionText, dataflowFromHere)
     }
 
     private fun showReachabilitySession(
-        session: ImplementationViewSession,
         editor: Editor,
         root: SliceNode,
         questionText: String,
         dataflowFromHere: Boolean
     ) {
-        if (session.implementationElements.isNotEmpty()) {
-            // TODO: come up with an actual title based on the element/expression under analysis
-            session.implementationElements.forEach {
-                logger.info("ViewElement Impl: ${it.elementForShowUsages}")
+        var popup = SoftReference.dereference(popupRef)
+
+        val project = editor.project ?: return
+
+        val viewComponent =
+            object :
+                ReachabilityPanel(
+                    project,
+                    dataflowFromHere,
+                    root,
+                    false,
+                ) {
+                override var isAutoScroll: Boolean = false
             }
-            var popup = SoftReference.dereference(popupRef)
-            popup?.takeIf { it.isVisible && it is AbstractPopup }?.let {
-                val viewComponent = (it as AbstractPopup).component as? ImplementationViewComponent
-                // Does the index need to be 0 or 1?
-                viewComponent?.update(session.implementationElements, 0)
+        val updateProcessor =
+            object : PopupUpdateProcessor(editor.project) {
+                override fun updatePopup(lookupItemObject: Any?) {
+                    // TODO: implement this?
+                }
             }
 
-            val viewComponent =
-                object :
-                    ReachabilityPanel(
-                        session.project,
-                        dataflowFromHere,
-                        root,
-                        false,
-                    ) {
-                    override var isAutoScroll: Boolean = false
-                }
-            val updateProcessor =
-                object : PopupUpdateProcessor(editor.project) {
-                    override fun updatePopup(lookupItemObject: Any?) {
-                        // TODO: implement this?
-                    }
-                }
+        viewComponent.setSize(420, 200)
+        val popupBuilder =
+            JBPopupFactory.getInstance()
+                .createComponentPopupBuilder(viewComponent, viewComponent)
+                .setProject(editor.project)
+                .addListener(updateProcessor)
+                .addUserData(updateProcessor)
+                .setTitle(questionText)
+                .setTitleIcon(IconManager.reachabilityIcon.let(::ActiveIcon))
+                .setDimensionServiceKey(
+                    editor.project,
+                    DocumentationManager.JAVADOC_LOCATION_AND_SIZE,
+                    false
+                )
+                .setResizable(true)
+                .setMovable(true)
+                .setRequestFocus(LookupManager.getActiveLookup(editor) != null)
 
-            viewComponent.setSize(420, 200)
-            val popupBuilder =
-                JBPopupFactory.getInstance()
-                    .createComponentPopupBuilder(viewComponent, viewComponent)
-                    .setProject(editor.project)
-                    .addListener(updateProcessor)
-                    .addUserData(updateProcessor)
-                    .setTitle(questionText)
-                    .setTitleIcon(IconManager.reachabilityIcon.let(::ActiveIcon))
-                    .setDimensionServiceKey(
-                        editor.project,
-                        DocumentationManager.JAVADOC_LOCATION_AND_SIZE,
-                        false
-                    )
-                    .setResizable(true)
-                    .setMovable(true)
-                    .setRequestFocus(LookupManager.getActiveLookup(session.editor) != null)
-
-            popup = popupBuilder.createPopup()
-            PopupPositionManager.positionPopupInBestPosition(
-                popup,
-                session.editor,
-                DataManager.getInstance().getDataContext()
-            )
-            popupRef = WeakReference(popup)
-        }
+        popup = popupBuilder.createPopup()
+        PopupPositionManager.positionPopupInBestPosition(
+            popup,
+            editor,
+            DataManager.getInstance().getDataContext()
+        )
+        popupRef = WeakReference(popup)
     }
 }
--- src/main/kotlin/com/github/jyoo980/reachhover/services/tree/TreeTransformer.kt
+++
@@ -1,8 +0,0 @@
-package com.github.jyoo980.reachhover.services.tree
-
-import com.github.jyoo980.reachhover.model.Tree
-
-interface TreeTransformer<A, B> {
-
-    fun transform(tree: Tree<A>): Tree<B>
-}
