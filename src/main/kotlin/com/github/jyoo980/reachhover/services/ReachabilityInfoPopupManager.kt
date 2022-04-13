3a94bb95ec354d77b5615b364d53d7792f7d7f63
James Yoo <jyoo980@gmail.com>
2022-04-13 10:16:37 -0700

    Cleaning up code
      * Removing unused `logging` instances.
      * Factoring out maximum path length parameter to a constant field in
        `PresentationUtil.kt`.

--- src/main/kotlin/com/github/jyoo980/reachhover/services/ReachabilityInfoPopupManager.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/services/ReachabilityInfoPopupManager.kt
@@ -2,14 +2,12 @@ package com.github.jyoo980.reachhover.services
 
 import com.github.jyoo980.reachhover.model.ReachabilityHoverContext
 import com.github.jyoo980.reachhover.ui.ReachabilityPopupBuilder
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import java.lang.ref.WeakReference
 
 class ReachabilityInfoPopupManager {
 
-    private val logger: Logger = Logger.getInstance(ReachabilityInfoPopupManager::class.java)
     private val reachabilityPopupBuilder: ReachabilityPopupBuilder = ReachabilityPopupBuilder()
     private var currentContext: ReachabilityHoverContext? = null
     private var currentPopupRef: WeakReference<JBPopup>? = null
