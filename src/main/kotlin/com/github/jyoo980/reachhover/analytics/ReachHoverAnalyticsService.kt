f90586b4b68e0ad7bcd6f78625f24fa44f5c8e06
James Yoo <jyoo980@gmail.com>
2022-04-25 17:54:41 -0700

    Adding more analytics
      * `reach-hover` specific actions are now being recorded via the plugin.
      * To investigate: sometimes events aren't being captured... look at this
        again with other projects.

--- src/main/kotlin/com/github/jyoo980/reachhover/analytics/ReachHoverAnalyticsService.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/analytics/ReachHoverAnalyticsService.kt
@@ -1,5 +1,9 @@
 package com.github.jyoo980.reachhover.analytics
 
+import com.github.jyoo980.reachhover.analytics.AnalyticsValues.builtInBackwardAnalysisText
+import com.github.jyoo980.reachhover.analytics.AnalyticsValues.builtInForwardAnalysisText
+import com.github.jyoo980.reachhover.analytics.AnalyticsValues.reachHoverBackwardPrefix
+import com.github.jyoo980.reachhover.analytics.AnalyticsValues.reachHoverForwardPrefix
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.actionSystem.impl.ActionMenuItem
@@ -8,13 +12,14 @@ import java.awt.AWTEvent
 import java.awt.Toolkit
 import java.awt.event.AWTEventListener
 import java.awt.event.MouseEvent
+import javax.swing.JButton
 
 class ReachHoverAnalyticsService : AWTEventListener, AnActionListener, Disposable {
 
     private val logger: Logger = Logger.getInstance(ReachHoverAnalyticsService::class.java)
-    private val builtInBackwardAnalysisText = "Data Flow to Here..."
-    private val builtInForwardAnalysisText = "Data Flow from Here..."
     private var numTimesDataflowInvoked = 0
+    private var numTimesReachHoverInvoked = 0
+    private var prevActionChain: MutableList<AWTEvent> = mutableListOf()
 
     init {
         val eventMask =
@@ -25,14 +30,24 @@ class ReachHoverAnalyticsService : AWTEventListener, AnActionListener, Disposabl
     }
 
     override fun eventDispatched(event: AWTEvent?) {
-        event?.let { e ->
-            logger.info("EVENT: $e")
-            val mouseEvent = e as? MouseEvent
-            val isMousePressed = mouseEvent?.let { it.id == MouseEvent.MOUSE_PRESSED } ?: false
-            if (isMousePressed && isDataflowAnalysisEvent(e)) {
-                numTimesDataflowInvoked++
-                // For debugging.
-                logger.info("Dataflow Analysis invoked: $numTimesDataflowInvoked time(s)")
+        event?.let {
+            checkForDataflowAnalysisInvocation(it)
+            checkForReachHoverInvocation(it)
+        }
+    }
+
+    private fun checkForDataflowAnalysisInvocation(e: AWTEvent) {
+        val mouseEvent = e as? MouseEvent
+        val isMousePressed = mouseEvent?.let { it.id == MouseEvent.MOUSE_PRESSED } ?: false
+        if ((isDataflowAnalysisEvent(e) || isOkButtonClicked(e)) && isMousePressed) {
+            if (isDataflowAnalysisEvent(e)) {
+                prevActionChain.add(e)
+            } else if (isOkButtonClicked(e)) {
+                if (prevActionChain.any(::isDataflowAnalysisEvent)) {
+                    numTimesDataflowInvoked++
+                    logger.info("Dataflow Analysis invoked: $numTimesDataflowInvoked time(s)")
+                    prevActionChain.clear()
+                }
             }
         }
     }
@@ -46,5 +61,24 @@ class ReachHoverAnalyticsService : AWTEventListener, AnActionListener, Disposabl
             it.text == builtInBackwardAnalysisText || it.text == builtInForwardAnalysisText
         }
             ?: false
+    }
+
+    private fun checkForReachHoverInvocation(e: AWTEvent) {
+        val mouseEvent = e as? MouseEvent
+        val isMousePressed = mouseEvent?.let { it.id == MouseEvent.MOUSE_PRESSED } ?: false
+        if (isReachHoverEvent(e) && isMousePressed) {
+            numTimesReachHoverInvoked++
+            logger.info("ReachHover invoked: $numTimesReachHoverInvoked time(s)")
+        }
+    }
+
+    private fun isReachHoverEvent(event: AWTEvent): Boolean {
+        val eventText = (event.source as? JButton)?.text
+        return eventText?.contains(reachHoverBackwardPrefix)
+            ?: false || eventText?.contains(reachHoverForwardPrefix) ?: false
+    }
+
+    private fun isOkButtonClicked(event: AWTEvent): Boolean {
+        return (event.source as? JButton)?.let { it.text == "OK" } ?: false
     }
 }
