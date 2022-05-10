c1c62b1219b761e10bdcea5ad22a68da54f82244
James Yoo <jyoo980@gmail.com>
2022-05-10 14:47:55 -0700

    Adding task start/stop buttons
      * "Run Actions" toolbar now has buttons that a user can select whenever they
        start or stop a task.
      * This is logged to the `idea.log` file.

--- src/main/kotlin/icons/IconManager.kt
+++ src/main/kotlin/icons/IconManager.kt
@@ -6,4 +6,6 @@ object IconManager {
 
     val reachabilityIcon = IconLoader.getIcon("/icons/reachability.svg", IconManager::class.java)
     val documentationIcon = IconLoader.getIcon("/icons/documentation.svg", IconManager::class.java)
+    val completeTaskIcon = IconLoader.getIcon("/icons/task-complete.svg", IconManager::class.java)
+    val startTaskIcon = IconLoader.getIcon("/icons/task-start.svg", IconManager::class.java)
 }
