99bfcb1b775442dbafe5c0e0704909808259febc
James Yoo <jyoo980@gmail.com>
2022-05-19 09:11:47 -0700

    Adding time formatting to reach-hover logs
      * Modifying the `TimeUtil` interface to prepend logs with a timestamp in the
        format: `YYYY-MM-DD'T'HH:mm:ss`

--- src/main/kotlin/com/github/jyoo980/reachhover/analytics/TimeUtil.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/analytics/TimeUtil.kt
@@ -2,6 +2,7 @@ package com.github.jyoo980.reachhover.analytics
 
 import java.sql.Date
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.time.Instant
 
 interface TimeUtil {
@@ -9,6 +10,7 @@ interface TimeUtil {
     fun humanReadableDate(ms: Instant): String {
         val currentTimestamp = Timestamp(ms.toEpochMilli())
         val date = Date(currentTimestamp.time)
-        return date.toString()
+        val fmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        return fmt.format(date)
     }
 }
