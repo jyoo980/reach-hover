558ec3d12d819e0d8463fb2f47d578468c0efb95
James Yoo <jyoo980@gmail.com>
2022-06-21 13:23:39 -0700

    Modifying analytics for file names

--- src/main/kotlin/com/github/jyoo980/reachhover/analytics/FileListener.kt
+++ src/main/kotlin/com/github/jyoo980/reachhover/analytics/FileListener.kt
@@ -6,15 +6,11 @@ import com.intellij.openapi.vfs.VirtualFile
 
 class FileListener : FileEditorManagerListener {
 
-    // Should be okay to collect which files are open, given we provide the project in the
-    // experiment, but I'm going to obfuscate the name of the file by just hashing its string rep.
     override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
-        val hashedFileName = file.name.hashCode()
-        LogWriter.write("Opened $hashedFileName", EventType.FILE_OPEN)
+        LogWriter.write("Opened ${file.name}", EventType.FILE_OPEN)
     }
 
     override fun fileClosed(source: FileEditorManager, file: VirtualFile) {
-        val hashedFileName = file.name.hashCode()
-        LogWriter.write("Closed '$hashedFileName'", EventType.FILE_CLOSED)
+        LogWriter.write("Closed '${file.name}'", EventType.FILE_CLOSED)
     }
 }
